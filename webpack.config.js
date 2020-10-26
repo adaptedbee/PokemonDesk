3a7c062d9ff5bc05d22cebbc7041cd47b51d7a8a
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-10-26 20:59:58 +0300

    01.02 - Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+  mode: NODE_ENV ? NODE_ENV : 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: ['ts-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    })
+  ]
+};
