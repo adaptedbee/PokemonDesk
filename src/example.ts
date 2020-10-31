ef3d1f84031281490ea2c1c4cd667e64d8c9ee32
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-10-31 19:02:17 +0300

    Task 2

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,25 @@
-function concat(a: string, b: string): string {
-  return `${a}${b}`;
+// Task 1
+
+function concat(first: string, second: string): string {
+  return `${first}${second}`;
 }
 
 concat('Hello ', 'World'); // -> Hello World;
+
+// Task 2
+
+type NumberStringArray = Array<number|string>;
+
+interface Data {
+  [n: string]: string | NumberStringArray;
+}
+
+interface MyHomeTaskInterface {
+  [n: string]: string | NumberStringArray | Array<Data>;
+};
+
+const MyHometask: MyHomeTaskInterface = {
+  howIDoIt: "I Do It Wel",
+  someArray: ["string one", "string two", 42],
+  withData: [{ howIDoIt: "I Do It Wel", someArray: ["string one", 23] }],
+};
