036592309d46bb212f93ea4512ae66914d12ce6a
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-11-02 20:02:00 +0300

    03.02 - ESLint, Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import cn from 'classnames';
 import s from './App.module.scss';
 
 const App = () => {
-  return (
-    <div className={cn(s.header, 'color')}>
-      Yes, We Did It! This is App Component!
-    </div>
-  )
-}
+  return <div className={cn(s.header, 'color')}>Yes, We Did It! This is App Component!</div>;
+};
 
 export default App;
