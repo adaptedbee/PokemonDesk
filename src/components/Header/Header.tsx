1dcbbc98bb5d102bc73be55b122b3396ae573218
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-11-03 18:22:12 +0300

    Header - разметка

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// import s from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <a className="header__logo" href="#">
+          <img src="" width="157" height="63" alt="" />
+        </a>
+
+        <ul className="header__menu">
+          <li>
+            <a className="header__menu-link header__menu-link--active" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="header__menu-link" href="#">
+              Pokédex
+            </a>
+          </li>
+          <li>
+            <a className="header__menu-link" href="#">
+              Legendaries
+            </a>
+          </li>
+          <li>
+            <a className="header__menu-link" href="#">
+              Documentation
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
